4465bd20b4438c883ac0e194779353a6bfddd3f6
Anirudha Sarma Tumuluri <at1341@scarletmail.rutgers.edu>
2024-05-17 15:21:32 -0400

    added a tailwind config for later

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('homepage.html')
+    if request.method == 'POST':
+        workout = 'workout' in request.form
+        leetcode = 'leetcode' in request.form
+        cs_concepts = 'cs_concepts' in request.form
+        journal = 'journal' in request.form
+        moisturize = 'moisturize' in request.form
+        minoxidil = 'minoxidil' in request.form
+        thoughts = request.form.get('thoughts')
+        print(request.form)
+
+        # For demonstration purposes, we'll just print the values to the console
+        print(f"Workout: {workout}")
+        print(f"Leetcode: {leetcode}")
+        print(f"CS Concepts Learning: {cs_concepts}")
+        print(f"Journal: {journal}")
+        print(f"Moisturize before bed: {moisturize}")
+        print(f"Minoxidil: {minoxidil}")
+        print(f"Thoughts: {thoughts}")
+
+        # You can add code here to process the data as needed
+        
+        return redirect(url_for('home'))
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
